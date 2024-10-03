5b5f12085f3408544dd2e2b7963c99ea990f07be
Shadrack Otieno <shadrackmeshack7@gmail.com>
2024-10-03 12:24:36 -0700

    first commit

---
+++ learn.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>learning php</title>
+</head>
+<body>
+    <?php  echo("hello world");
+     echo "<h2>Dynamic Content</h2>";
+     echo "<p>Today's date is: " . date("Y-m-d") . "</p>";
+
+    ?>
+</body>
+</html>
