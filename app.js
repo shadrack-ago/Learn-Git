0a78a6fec5c185c917430d65b231e0c16215b112
Shadrack Otieno <shadrackmeshack7@gmail.com>
2024-10-04 06:33:02 -0700

    just diving into JS

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log(22);
+console.log(22); 
+// logging an istance to console 
+console.log("Shadrack "+ "Otieno"); //this is concatination of string and print to console
+console.log("shadrack".length); /* Objects
+, including instances of data types, can have properties, stored information. 
+The properties are denoted with a . after the name of the object, for example: 'Hello'.length. */
+console.log(`hello`.toUpperCase());
+console.log("shad" + 1)
