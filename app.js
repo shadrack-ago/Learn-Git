49b67361c69676ab2812379e3aa6fbb4626f25cd
Shadrack Otieno <shadrackmeshack7@gmail.com>
2024-10-05 11:12:20 -0700

    more training

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ console.log("shadrack".length); /* Objects
 The properties are denoted with a . after the name of the object, for example: 'Hello'.length. */
 console.log(`hello`.toUpperCase());
 console.log("shad" + 1)
+
+
+
+var a="shady";
+let b=" Ago"
+console.log(a + b);
+// Interpolate multiple variables into a string
+const e = "otieno";
+const f = " Ago";
+const g = 23;
+let info = `my name is ${e} ${f} I am of age ${g}`;
+console.log(info);
+
+
+let name = "Shadrack";
+let age = 25;
+let location = "Kenya";
+
+let message = `My name is ${name}, I am ${age} years old, and I live in ${location}.`;
+
+console.log(message);
+
